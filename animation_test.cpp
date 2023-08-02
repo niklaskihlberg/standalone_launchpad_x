9f6b3e501a1a9b6ada9eb509b4424870ddb43a7b
nklz <47744294+niklaskihlberg@users.noreply.github.com>
2023-08-02 16:09:54 +0200

    Update animation_test.cpp

--- animation_test.cpp
+++ animation_test.cpp
@@ -82,5 +82,28 @@
                68,77
                   78,87
 
+// test
+unsigned long a_time = 125;
+unsigned long multi = 0;
+if(currentMillis - previousMillis >= (a_time * multi)){ multi ++}
+if(currentMillis - previousMillis >= (a_time * )){}
+
+
+// template
+81,   82,   83,   84,   85,   86,   87,   88
+
+71,   72,   73,   74,   75,   76,   77,   78
+
+61,   62,   63,   64,   65,   66,   67,   68
+
+51,   52,   53,   54,   55,   56,   57,   58
+
+41,   42,   43,   44,   45,   46,   47,   48
+
+31,   32,   33,   34,   35,   36,   37,   38
+
+21,   22,   23,   24,   25,   26,   27,   28
+
+11,   12,   13,   14,   15,   16,   17,   18
 
 
