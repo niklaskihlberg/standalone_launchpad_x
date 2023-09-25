419822ffbc5a8b67773059abe2b040c8cc4c495d
nklz <47744294+niklaskihlberg@users.noreply.github.com>
2023-09-25 21:08:01 +0200

    Update sequencer.cpp

--- sequencer.cpp
+++ sequencer.cpp
@@ -28,4 +28,18 @@ for (uint8_t bar = 0      ; bar < 1      ; bar++      ) {
     }
 };
 
+if (millis() - noiasca_millis >= a_time) {
+    state++;
+    state = state % 33;
+    Serial << endl << "——————————————————————————— State: " << state << endl;
+
+    switch (state) {
+      case 0:  for (uint8_t pad = 0; pad < 1; pad++) { send_led_sysex_to_one_pad(anim01[pad], color_lines[0], color_lines[1], color_lines[2]); };     break;
+      
+      case 1:  for (uint8_t pad = 0; pad < 2; pad++) { send_led_sysex_to_one_pad(anim02[pad], color_lines[0], color_lines[1], color_lines[2]); };                                                                                                                                                                                                                                                                                                                                                                   send_led_sysex_to_one_pad(18, c0[0],c0[1],c0[2]); break;
+      case 2:  for (uint8_t pad = 0; pad < 3; pad++) { send_led_sysex_to_one_pad(anim03[pad], color_lines[0], color_lines[1], color_lines[2]); };                                                                                                                                                                                                                                                                                                                 send_led_sysex_to_one_pad(28, c0[0],c0[1],c0[2]); send_led_sysex_to_one_pad(17, c0[0],c0[1],c0[2]); break;
+      case 3:  for (uint8_t pad = 0; pad < 4; pad++) { send_led_sysex_to_one_pad(anim04[pad], color_lines[0], color_lines[1], color_lines[2]); };                                                                                                                                                                                                                                                               send_led_sysex_to_one_pad(38, c0[0],c0[1],c0[2]); send_led_sysex_to_one_pad(27, c0[0],c0[1],c0[2]); send_led_sysex_to_one_pad(16, c0[0],c0[1],c0[2]); break;
+      case 4:  for (uint8_t pad = 0; pad < 5; pad++) { send_led_sysex_to_one_pad(anim05[pad], color_lines[0], color_lines[1], color_lines[2]); };                                                                                                                                                                                                             send_led_sysex_to_one_pad(48, c0[0],c0[1],c0[2]); send_led_sysex_to_one_pad(37, c0[0],c0[1],c0[2]); send_led_sysex_to_one_pad(26, c0[0],c0[1],c0[2]); send_led_sysex_to_one_pad(15, c0[0],c0[1],c0[2]); break;
+      
+
  
